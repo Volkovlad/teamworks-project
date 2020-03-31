d2f322f54ab7001f7c9598626db80794712d3976
LukashOleksii <43243011+LukashOleksii@users.noreply.github.com>
2020-03-31 15:09:31 +0300

    add header and color filter

--- MoveProject/src/app/app.module.ts
+++ MoveProject/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { BrandFilterComponent } from '../components/brand-filter/brand-filter.component';
 import { ListOfProductsComponent } from '../components/list-of-products/list-of-products.component';
+import {HeaderComponent} from '../components/header/header.component';
+import {ColorFilterComponent} from '../components/color-filter/color-filter.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ListOfProductsComponent } from '../components/list-of-products/list-of-
     AppComponent,
     FooterComponent,
     BrandFilterComponent,
-    ListOfProductsComponent
+    ListOfProductsComponent,
+    HeaderComponent,
+    ColorFilterComponent
   ],
   imports: [
     BrowserModule,
