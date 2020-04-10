918ed814c82ab89bb0bc2bbb8981e61f9be9f6c5
LukashOleksii <43243011+LukashOleksii@users.noreply.github.com>
2020-04-10 23:21:28 +0300

    create database

--- moveBE/BEforDB/admin.py
+++ moveBE/BEforDB/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Shoe
 
+from .models import Shoe, User, Order, OrderList, Comment, Favorite, Color, Image, Size
 
 admin.site.register(Shoe)
-
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Comment)
+admin.site.register(OrderList)
+admin.site.register(Favorite)
+admin.site.register(Color)
+admin.site.register(Image)
+admin.site.register(Size)
 # Register your models here.
--- moveBE/BEforDB/models.py
+++ moveBE/BEforDB/models.py
@@ -5,8 +5,89 @@ class Shoe(models.Model):
     brand = models.CharField(max_length=30)
     model = models.CharField(max_length=30)
     price = models.IntegerField()
-    color = models.CharField(max_length=25)
-    size = models.IntegerField()
 
     def __str__(self):
-        return self.model
+        return str(self.brand +" "+ self.model)
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    name = models.CharField(max_length=30)
+    phone = models.IntegerField()
+    date = models.DateField()
+    payment_status = models.BooleanField()
+    order_status = models.CharField(max_length=20)
+    user_shoe_status = models.BooleanField()
+
+    def __str__(self):
+        return self.order_status + " - "+ str(self.date)
+
+
+
+class Comment(models.Model):
+    description = models.CharField(max_length=300)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.shoe)
+
+
+class Color(models.Model):
+    color = models.CharField(max_length=20)
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.shoe) +" - "+ self.color
+
+
+class Image(models.Model):
+    image = models.CharField(max_length=300, blank=True)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.color
+    def __str__(self):
+        return str(self.color)
+
+
+class Size(models.Model):
+    size = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+    def __int__(self):
+        return self.size
+    def __str__(self):
+        return str(self.color) +" - "+ str(self.size)
+
+class OrderList(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    shoe = models.ForeignKey(Size, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+    def __str__(self):
+        return str(self.shoe)+" - "+ str(self.count)
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shoe = models.ForeignKey(Size, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.user)+" - "+ str(self.shoe)
