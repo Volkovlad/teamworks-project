33f19bb19213e2d4feb4f738bead57b43b080fc0
BohdanVolskyi <43409694+BohdanVolskyi@users.noreply.github.com>
2020-04-14 16:25:17 +0300

    create routing and fix some bugs

--- move-shop-client/src/app/app-routing.module.ts
+++ move-shop-client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FaqComponent} from './modules/faq/faq.component';
 
 const routes: Routes = [
   {
